49c0936916567a8aa4afc1baa57a9bd49ad52f4c
jomora <jomora@1d73c0e2-6aed-3c4a-a06a-98d8c4972a2a>
2009-05-24 09:59:22 +0000

    20090101

---
+++ WallpaperSetter/trunk/WallpaperSetter/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Net;
+using System.IO;
+using System.Web;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace WallpaperSetter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //InitializeComponent();
+            string filePath;
+            Bitmap bmp = GetImage(out filePath);
+            LogEvent(filePath);
+            Cycler.WindowsAPI.SetDesktopBackground(bmp,
+                Cycler.DesktopBackgroundStyle.Centered);
+        }
+
+        static Bitmap GetImage(out string filePath)
+        {
+            string image_url = "http://jomura.net/picture/random";
+            string adult = ConfigurationSettings.AppSettings["adult"];
+            if (!string.IsNullOrEmpty(adult) && Boolean.Parse(adult))
+            {
+                image_url += "?adult";
+            }
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add("Referer", "http://jomura.net/picture/");
+                using (Stream imageStrm = wc.OpenRead(image_url))
+                {
+                    filePath = Uri.UnescapeDataString(
+                        wc.ResponseHeaders["PictureFilePath"]);
+                    return new Bitmap(imageStrm);
+                }
+            }
+        }
+
+        static void LogEvent(string filePath)
+        {
+            string sSource = System.Reflection.Assembly
+                .GetExecutingAssembly().FullName;
+            string sLog = "Application";
+            string sEvent = "Changed wallpaper into \"" + filePath + "\"";
+
+            if (!EventLog.SourceExists(sSource))
+            {
+                EventLog.CreateEventSource(sSource, sLog);
+            }
+
+            EventLog.WriteEntry(sSource, sEvent);
+        }
+    }
+}
---
+++ WallpaperSetter/trunk/WallpaperSetter/Properties/SuppressMessages.cs
@@ -0,0 +1,7 @@
+﻿#if CODE_ANALYSIS
+using System.Diagnostics.CodeAnalysis;
+
+[module: SuppressMessage("Microsoft.Usage", "CA1806:DoNotIgnoreMethodResults", Scope = "member", Target = "WallpaperSetter.Program.#Main()", MessageId = "WallpaperSetter.Form1")]
+[module: SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Scope = "member", Target = "WallpaperSetter.Form1.#LogEvent(System.String)")]
+
+#endif
